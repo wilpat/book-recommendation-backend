9f51d039d1b1fc6dab31a29dc565e1c79091abb6
William Okafor <wilpat456@gmail.com>
2019-10-02 23:43:55 +0100

    Backend CRUD requests complete.

--- app/Category.php
+++ app/Category.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App;
+use App\Book;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $guarded = [];
+
     public function books()
     {
-        return $this->hasMany(Books::class);
+        return $this->hasMany(Book::class);
     }
 }
